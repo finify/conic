c7af6d377f90309842f86880546e4de537cdcff5
finify <ifeanyiizuegbu@yahoo.com>
2022-08-09 15:32:53 +0100

    testing deploy

--- app/Views/Layouts/Loginlayout.php
+++ app/Views/Layouts/Loginlayout.php
@@ -14,7 +14,7 @@
       content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
     />
 
-    <title><?= $this->siteTitle?></title>
+    <title>11<?= $this->siteTitle?></title>
 
     <meta name="description" content="" />
 
